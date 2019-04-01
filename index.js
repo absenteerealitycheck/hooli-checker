3b0b4718d9ee3ef006834cc9a472faab1123993d
Lexie <againessmith@gmail.com>
2019-04-01 19:13:37 -0400

    Task Completed

--- index.js
+++ index.js
@@ -1,8 +1,53 @@
 const fs = require('fs'); // useful for navigating the file system
 const parse = require('csv-parse/lib/sync'); // needed for parsing CSV file data
+const async = require('async');
 
 function linkBuyerToFacility() {
-  // your solution goes here
+	let fileOutputs = {};
+	async.forEachOf(
+	   {existing: './existing-accounts.csv', sams: './sam-accounts.csv'},
+	   function(value,key, cb) {
+	     fs.readFile(value, function(err, content) {
+	       if (!err) {
+	       const records = parse(content, {
+	 					columns: true,
+						skip_empty_lines: true
+			});
+				
+	         fileOutputs[key] = records;
+
+	       }
+	       cb(err);
+	     });
+	   },
+	   function(err) {
+	   	let samsSet,
+	   		existingSet,
+	   		resultsSet;
+	    if(!err){
+	     	existingSet = new Set(fileOutputs.existing.map((value)=>{
+	     		return value.hooliId.slice(0,-3);
+
+	     	}));
+	     	samsSet = new Set(fileOutputs.sams.map((value) => {
+	     		return value.accountHooliId;
+	     	}));
+	     	
+	     	resultsSet = JSON.stringify(Array.from(new Set([...samsSet].filter(x=>!existingSet.has(x)))), null, '\n').replace(/^\[|]$/g, '');
+	     	
+	     	console.log(resultsSet);
+	    	
+	    	return fs.writeFile("IDsNotInDatabase.txt", resultsSet, function(err) {
+	   			if(err) {
+	       			return console.log(err);
+	   			}
+	  			console.log("The results have been saved!");
+
+		});}
+	    return console.log(err);
+	   }
+    
+  );
 }
 
 linkBuyerToFacility();
